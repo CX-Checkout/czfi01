1dac7fbabca6176db8b578086a7a9bac7ee2e982
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 21:04:58 +0000

    Sun Dec 03 21:04:58 GMT 2017

--- test/solutions/checkoutShould.js
+++ test/solutions/checkoutShould.js
@@ -1,6 +1,11 @@
-var sum = require('../../lib/solutions/checkout');
+var checkout = require('../../lib/solutions/checkout');
 
-exports['compute sum'] = function (test) {
-    test.equal(sum(1, 2), 3);
+exports['return -1 for a invalid input'] = function (test) {
+    test.equal(checkout(''), -1);
     test.done();
 };
+
+exports['returns the price of the product if we pass only one sku'] = function(test) {
+    test.equal(checkout('A'), 50);
+    test.done();
+}
