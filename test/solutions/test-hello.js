a79cc2fad5647c983415134780d2054f0ae2a46d
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 20:56:43 +0000

    Sun Dec 03 20:56:43 GMT 2017

--- test/solutions/test-hello.js
+++ test/solutions/test-hello.js
@@ -1,6 +1,6 @@
 var hello = require('../../lib/solutions/hello');
 
 exports['say hello'] = function (test) {
-    test.equal(hello('world'), 'hello world');
+    test.equal(hello('world'), 'Hello, World!');
     test.done();
 };
