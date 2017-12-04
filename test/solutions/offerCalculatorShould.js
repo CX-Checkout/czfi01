15805973f3d20f122df9720a0fb936d0de77b800
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 21:58:28 +0000

    Mon Dec 04 21:58:28 GMT 2017

--- test/solutions/offerCalculatorShould.js
+++ test/solutions/offerCalculatorShould.js
@@ -16,5 +16,6 @@ exports['returns the offer price if we have any offer in the skus list with prod
 }
 
 exports['returns the offer price when we have multiple offers in the skus list'] = function(test) {
-    test.equal(['A', 'A', 'A','A', 'A', 'A'])
+    test.equal(['A', 'A', 'A','A', 'A', 'A'], 260);
+    test.done();
 }
