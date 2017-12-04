499eb1b1a03e4decc462dfef13aee5a618be859d
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 09:55:51 +0000

    Mon Dec 04 09:55:51 GMT 2017

--- test/solutions/checkoutShould.js
+++ test/solutions/checkoutShould.js
@@ -5,17 +5,22 @@ exports['return -1 for a invalid input'] = function (test) {
     test.done();
 };
 
-exports['returns the price of the product if we pass only one sku'] = function(test) {
+exports['returns the price of the product if it pass only one sku'] = function(test) {
     test.equal(checkout('A'), 50);
     test.done();
 }
 
-exports['return the sum of all skus we have in our shop if it don\'t have any offer'] = function(test) {
+exports['return the sum of all skus it have in the shop if it don\'t have any offer'] = function(test) {
     test.equal(checkout('A,B,C,D'), 115);
     test.done();
 }
 
-exports['return -1 if we have an invalid sku in the list'] = function(test) {
+exports['return -1 if it have an invalid sku in the list'] = function(test) {
     test.equal(checkout('A,H,J'), -1);
+    test.done();
+}
+
+exports['return the offer price if it have an offer in the list'] = function(test) {
+    test.equal(checkout('A,A,A'), 130);
     test.done();
 }
