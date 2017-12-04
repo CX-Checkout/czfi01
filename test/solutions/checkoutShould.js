33bffbeca2833575f3337a6b5a84d06c904859f6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 09:43:51 +0000

    Mon Dec 04 09:43:51 GMT 2017

--- test/solutions/checkoutShould.js
+++ test/solutions/checkoutShould.js
@@ -10,11 +10,6 @@ exports['returns the price of the product if we pass only one sku'] = function(t
     test.done();
 }
 
-exports['returns the sum of the all skus if it don\'t have any offer'] = function(test) {
-    test.equal(checkout('A,B'), 80);
-    test.done();
-}
-
 exports['return the sum of all skus we have in our shop if it don\'t have any offer'] = function(test) {
     test.equal(checkout('A,B,C,D'), 115);
     test.done();
