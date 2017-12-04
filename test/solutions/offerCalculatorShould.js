933abe222be8b0fc54b6d0c7ea0c94dd7834b401
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 21:37:28 +0000

    Mon Dec 04 21:37:28 GMT 2017

--- test/solutions/offerCalculatorShould.js
+++ test/solutions/offerCalculatorShould.js
@@ -1,2 +1,10 @@
-const calculateOffers = require('../../lib/solutions/offerCalculator');
+const calculateOfferPrice = require('../../lib/solutions/offerCalculator');
 
+exports['return 0 if skus list don\'t have any offer'] = function(test) {
+    test.equal(calculateOfferPrice([]), 0);
+    test.done();
+}
+
+exports['returns the offer price if we have any offer in the skus list'] = function(test) {
+       
+}
