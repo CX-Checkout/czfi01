7a89d8147cfa55158cb95a302362d56c85150f60
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 22:39:54 +0000

    Mon Dec 04 22:39:54 GMT 2017

--- test/solutions/offerProductsRemoverShould.js
+++ test/solutions/offerProductsRemoverShould.js
@@ -4,3 +4,13 @@ exports['return the same array if it don\'t have offers'] = function(test) {
     test.equal(JSON.stringify(removeOfferedProducts([])), JSON.stringify([]));
     test.done();
 }
+
+exports['returns an empty array if it has one offer inside'] = function(test) {
+    test.equal(JSON.stringify(removeOfferedProducts(['A', 'A', 'A'])), JSON.stringify([]));
+    test.done();
+}
+
+exports['returns an array with only the unoffered skus'] = function(test) {
+    test.equal(JSON.stringify(['A', 'A', 'A', 'C']), JSON.stringify(['C']));
+    test.done();
+}
