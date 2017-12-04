6233434483eaa8353f5fdda128f9a51b10b24376
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 22:33:54 +0000

    Mon Dec 04 22:33:54 GMT 2017

--- test/solutions/offerProductsRemoverShould.js
+++ test/solutions/offerProductsRemoverShould.js
@@ -1,3 +1,6 @@
+const removeOfferedProducts = require('../../lib/solutions/offerProductsRemover');
+
 exports['return the same array if it don\'t have offers'] = function(test) {
-    test.equal()
+    test.equal(JSON.stringify(removeOfferedProducts([])), JSON.stringify([]));
+    test.done();
 }
