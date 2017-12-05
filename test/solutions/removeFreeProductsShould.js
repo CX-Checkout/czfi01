9cd56cc6341f2376b66fc86541a6dbac21fb8b25
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 09:34:06 +0000

    Tue Dec 05 09:34:06 GMT 2017

--- test/solutions/removeFreeProductsShould.js
+++ test/solutions/removeFreeProductsShould.js
@@ -16,6 +16,6 @@ exports['returns the array without the free products of our offers'] = function(
 }
 
 exports['returns the array without the free product of our offer'] = function(test) {
-    test.equal(JSON.stringify(removeFreeProducts(['B', 'E', 'E', 'C', 'F', 'F', 'F'])), JSON.stringify(['E', 'E', 'C', 'F', 'F']));
+    test.equal(JSON.stringify(removeFreeProducts(['N','N','N','M'])), JSON.stringify(['E', 'E', 'C', 'F', 'F']));
     test.done();
 }
