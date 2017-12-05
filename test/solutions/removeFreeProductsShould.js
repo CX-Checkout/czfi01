a58220c95ef60b85596da88fa72674710f091666
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 00:08:04 +0000

    Tue Dec 05 00:08:04 GMT 2017

--- test/solutions/removeFreeProductsShould.js
+++ test/solutions/removeFreeProductsShould.js
@@ -1,6 +1,11 @@
 var removeFreeProducts = require('../../lib/solutions/removeFreeProducts');
 
 exports['remove one B if we have two E'] = function(test) {
-    test.equal(removeFreeProducts(['B', 'E', 'E']), ['E', 'E']);
+    test.equal(JSON.stringify(removeFreeProducts(['B', 'E', 'E'])), JSON.stringify(['E', 'E']));
+    test.done();
+}
+
+exports['returns the array without the free products of our offer'] = function(test) {
+    test.equal(JSON.stringify(removeFreeProducts(['B', 'E', 'E', 'C'])), JSON.stringify(['E', 'E', 'C']));
     test.done();
 }
