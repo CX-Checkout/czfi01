9b28e6b667e8d1982c210a2cffdf2775370a4a65
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 15:14:33 +0000

    Tue Dec 05 15:14:33 GMT 2017

--- test/solutions/packProductsShould.js
+++ test/solutions/packProductsShould.js
@@ -5,7 +5,12 @@ exports['return an empty list if we have only one pack'] = function(test) {
     test.done();
 }
 
-exports['return a a new list without the packed products'] = function(test) {
+exports['returns a list with one product if we have one pack and one product'] = function(test) {
     test.equal(JSON.stringify(packProducts(['S', 'T', 'X', 'A'])), JSON.stringify(['A']));
+    test.done();
+}
+
+exports['returns a empty list if we have two packs inside'] = function() {
+    test.equal(JSON.stringify(packProducts(['S', 'T', 'X', 'S', 'T', 'X'])), JSON.stringify([]));
     test.done();
 }
